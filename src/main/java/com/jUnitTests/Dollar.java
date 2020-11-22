173379a4bc05c3ccc6edbe5aa1305a71270387a6
kamildyla <kamil.dyla09@gmail.com>
2020-11-22 18:38:20 +0100

    initialize

---
+++ src/main/java/com/jUnitTests/Dollar.java
@@ -0,0 +1,17 @@
+package com.jUnitTests;
+
+import lombok.Data;
+
+@Data
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount){
+        this.amount = amount;
+    }
+
+     void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
