65117b445949301a4769af39d314b4377c19e03f
kamildyla <kamil.dyla09@gmail.com>
2020-11-26 21:23:40 +0100

    made Money abstract class

--- src/main/java/com/jUnitTests/Money.java
+++ src/main/java/com/jUnitTests/Money.java
@@ -5,12 +5,23 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return (this.getAmount() == money.getAmount());
+        return (this.getAmount() == money.getAmount())
+                && (this.getClass().equals(object.getClass()));
     }
 }
