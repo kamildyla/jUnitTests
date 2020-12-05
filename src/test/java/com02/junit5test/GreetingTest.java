d7e807447173fa2429b831590afafe43c1500292
kamildyla <kamil.dyla09@gmail.com>
2020-12-05 22:32:35 +0100

    new package for new learning

---
+++ src/test/java/com02/junit5test/GreetingTest.java
@@ -0,0 +1,43 @@
+package com02.junit5test;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("before all once");
+    }
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+        System.out.println("beforeEach test");
+    }
+
+    @Test
+    void testHelloWorld1() {
+        assertEquals("Hello World", greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld2() {
+        String name = "Kamil";
+        assertEquals("Hello Kamil", greeting.helloWorld(name));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("aftre each test");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("after all once");
+    }
+
+}
